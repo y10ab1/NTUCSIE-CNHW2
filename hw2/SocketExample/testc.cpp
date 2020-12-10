31276ab81eb96fe0f13469cac7b8ce11ab6f7365
y10ab1 <b07902144@ntu.edu.tw>
2020-12-10 16:50:15 +0800

    sample

--- hw2/SocketExample/testc.cpp
+++ hw2/SocketExample/testc.cpp
@@ -1,39 +1,39 @@
-/**
- * OpenCV video streaming over TCP/IP
+/* OpenCV video streaming over TCP/IP
  * Client: Receives video from server and display it
  * by Steve Tuenkam
+ * 
+ * modified by Sheriff Olaoye (sheriffolaoye.com)
  */
 
 #include "opencv2/opencv.hpp"
-#include <sys/socket.h>
+#include <sys/socket.h> 
 #include <arpa/inet.h>
 #include <unistd.h>
 
 using namespace cv;
 
-int main(int argc, char **argv)
-{
+int QUIT = 1;
 
+int main(int argc, char** argv)
+{
     //--------------------------------------------------------
     //networking stuff: socket , connect
     //--------------------------------------------------------
-    int sokt;
-    char *serverIP;
-    int serverPort;
+    int         socket_fd;
+    char*       serverIP;
+    int         serverPort;
 
-    if (argc < 3)
-    {
-        std::cerr << "Usage: cv_video_cli <serverIP> <serverPort> " << std::endl;
+    if (argc < 3) {
+           std::cerr << "Usage: cv_video_cli <serverIP> <serverPort> " << std::endl;
     }
 
-    serverIP = argv[1];
+    serverIP   = argv[1];
     serverPort = atoi(argv[2]);
 
-    struct sockaddr_in serverAddr;
-    socklen_t addrLen = sizeof(struct sockaddr_in);
+    struct  sockaddr_in serverAddr;
+    socklen_t           addrLen = sizeof(struct sockaddr_in);
 
-    if ((sokt = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    if ((socket_fd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
         std::cerr << "socket() failed" << std::endl;
     }
 
@@ -41,8 +41,7 @@ int main(int argc, char **argv)
     serverAddr.sin_addr.s_addr = inet_addr(serverIP);
     serverAddr.sin_port = htons(serverPort);
 
-    if (connect(sokt, (sockaddr *)&serverAddr, addrLen) < 0)
-    {
+    if (connect(socket_fd, (sockaddr*)&serverAddr, addrLen) < 0) {
         std::cerr << "connect() failed!" << std::endl;
     }
 
@@ -51,37 +50,34 @@ int main(int argc, char **argv)
     //----------------------------------------------------------
 
     Mat img;
-    img = Mat::zeros(540, 960, CV_8UC3);
+    img = Mat::zeros(480 , 640, CV_8UC3);
     int imgSize = img.total() * img.elemSize();
     uchar *iptr = img.data;
     int bytes = 0;
     int key;
 
     //make img continuos
-    if (!img.isContinuous())
-    {
+    if ( ! img.isContinuous() ) { 
         img = img.clone();
     }
-
+        
     std::cout << "Image Size:" << imgSize << std::endl;
 
     namedWindow("CV Video Client", 1);
 
-    while (key != 'q')
-    {
-
-        if ((bytes = recv(sokt, iptr, imgSize, MSG_WAITALL)) == -1)
-        {
+    while (key != 'q') {
+        if ((bytes = recv(socket_fd, iptr, imgSize , MSG_WAITALL)) == -1) {
             std::cerr << "recv failed, received bytes = " << bytes << std::endl;
         }
-
+        
         cv::imshow("CV Video Client", img);
-        char c = (char)cv::waitKey(33.3333);
-        if (c == 27)
-            break;
+        if (key = cv::waitKey(10) >= 0) break;
     }
 
-    close(sokt);
+    // tell server to quit
+    send(socket_fd, (void *)QUIT, sizeof(QUIT), 0);
+
+    close(socket_fd);
 
     return 0;
 }
