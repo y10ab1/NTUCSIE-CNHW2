85e35e2dc5db1cf8b7edf9aa6d9159798d130e69
y10ab1 <b07902144@ntu.edu.tw>
2020-12-10 17:28:27 +0800

    pth

--- hw2/SocketExample/tests.cpp
+++ hw2/SocketExample/tests.cpp
@@ -8,11 +8,11 @@
 
 #include "opencv2/opencv.hpp"
 #include <iostream>
-#include <sys/socket.h> 
+#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/ioctl.h>
 #include <net/if.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <string.h>
 
 #include <semaphore.h>
@@ -29,8 +29,8 @@ int capDev = 0;
 // open the default camera
 VideoCapture cap("./tmp.mpg");
 
-int main(int argc, char** argv)
-{   
+int main(int argc, char **argv)
+{
     // initialize semaphore;
     sem_init(&semaphore, 0, 1);
 
@@ -39,63 +39,70 @@ int main(int argc, char** argv)
     //--------------------------------------------------------
     int localSocket,
         remoteSocket,
-        port = 4097;                               
-    struct  sockaddr_in localAddr,
-                        remoteAddr;
+        port = 4097;
+    struct sockaddr_in localAddr,
+        remoteAddr;
     int addrLen = sizeof(struct sockaddr_in);
 
     pthread_t send_thread[10];
     pthread_t recv_thread[10];
-       
-    if ( (argc > 1) && (strcmp(argv[1],"-h") == 0) ) {
-        std::cerr << "usage: ./cv_video_srv [port] [capture device]\n" <<
-                     "port           : socket port (4097 default)\n" <<
-                     "capture device : (0 default)\n" << std::endl;
+
+    if ((argc > 1) && (strcmp(argv[1], "-h") == 0))
+    {
+        std::cerr << "usage: ./cv_video_srv [port] [capture device]\n"
+                  << "port           : socket port (4097 default)\n"
+                  << "capture device : (0 default)\n"
+                  << std::endl;
 
         exit(1);
     }
 
-    if (argc == 2) port = atoi(argv[1]);
+    if (argc == 2)
+        port = atoi(argv[1]);
 
-    localSocket = socket(AF_INET , SOCK_STREAM , 0);
-    if (localSocket == -1){
+    localSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (localSocket == -1)
+    {
         perror("socket() call failed!!");
-    }    
+    }
 
     localAddr.sin_family = AF_INET;
     localAddr.sin_addr.s_addr = INADDR_ANY;
-    localAddr.sin_port = htons( port );
+    localAddr.sin_port = htons(port);
 
-    if( bind(localSocket,(struct sockaddr *)&localAddr , sizeof(localAddr)) < 0) {
+    if (bind(localSocket, (struct sockaddr *)&localAddr, sizeof(localAddr)) < 0)
+    {
         perror("Can't bind() socket");
         exit(1);
     }
-    
+
     // Listening
-    listen(localSocket , 3);
-    
-    std::cout <<  "Waiting for connections...\n"
-              <<  "Server Port:" << port << std::endl;
+    listen(localSocket, 3);
+
+    std::cout << "Waiting for connections...\n"
+              << "Server Port:" << port << std::endl;
 
     //accept connection from an incoming client
-    while(1){
-        remoteSocket = accept(localSocket, (struct sockaddr *)&remoteAddr, (socklen_t*)&addrLen);  
-        
-        if (remoteSocket < 0) {
+    while (1)
+    {
+        remoteSocket = accept(localSocket, (struct sockaddr *)&remoteAddr, (socklen_t *)&addrLen);
+
+        if (remoteSocket < 0)
+        {
             perror("accept failed!");
             exit(1);
-        } 
-        
-        std::cout << "Connection accepted" << std::endl;
-        pthread_create(&send_thread[remoteSocket-2], NULL, send_data, &remoteSocket);
-        pthread_create(&recv_thread[remoteSocket-2], NULL, recv_data, &remoteSocket);
+        }
+
+        std::cout << "Connection accepted: " << remoteSocket << std::endl;
+        pthread_create(&send_thread[remoteSocket - 2], NULL, send_data, &remoteSocket);
+        pthread_create(&recv_thread[remoteSocket - 2], NULL, recv_data, &remoteSocket);
 
         // pthread_join(thread_id, NULL);
     }
 
     // wait for threads to finish up
-    pthread_join(send_thread[remoteSocket-2], NULL);
-    pthread_join(recv_thread[remoteSocket-2], NULL);
+    pthread_join(send_thread[remoteSocket - 2], NULL);
+    pthread_join(recv_thread[remoteSocket - 2], NULL);
 
     // close the socket
     close(remoteSocket);
@@ -103,7 +110,8 @@ int main(int argc, char** argv)
     return 0;
 }
 
-void *send_data(void *ptr){
+void *send_data(void *ptr)
+{
     int socket = *(int *)ptr;
     //OpenCV Code
     //----------------------------------------------------------
@@ -122,27 +130,31 @@ void *send_data(void *ptr){
     int bytes = 0;
 
     // make img continuos
-    if(!img.isContinuous()){ 
+    if (!img.isContinuous())
+    {
         img = img.clone();
     }
     std::cout << "Image Size:" << imgSize << std::endl;
 
     bool status = true;
 
-    while(status){
+    while (status)
+    {
         // get a frame from the camera
         cap >> img;
         // flip the frame
         flip(img, flippedFrame, 1);
 
         // send the flipped frame over the network
-        if ((bytes = send(socket, flippedFrame.data, imgSize, 0)) < 0){
+        if ((bytes = send(socket, flippedFrame.data, imgSize, 0)) < 0)
+        {
             std::cerr << "bytes = " << bytes << std::endl;
             break;
         }
 
         sem_wait(&semaphore);
-        if(running) status = false;
+        if (running)
+            status = false;
         sem_post(&semaphore);
     }
 
@@ -150,21 +162,26 @@ void *send_data(void *ptr){
     close(socket);
 }
 
-void *recv_data(void *ptr){
+void *recv_data(void *ptr)
+{
     int socket = *(int *)ptr;
     char *buffer;
     int bytes;
 
-    while(1){
+    while (1)
+    {
         bytes = recv(socket, buffer, sizeof(int), MSG_WAITALL);
 
-        if (bytes < 0){
+        if (bytes < 0)
+        {
             std::cerr << "error receiving from client" << std::endl;
         }
-        else if (bytes > 0){
+        else if (bytes > 0)
+        {
             int msg = atoi(buffer);
 
-            if (msg == QUIT){
+            if (msg == QUIT)
+            {
                 sem_wait(&semaphore);
                 running = true;
                 sem_post(&semaphore);
