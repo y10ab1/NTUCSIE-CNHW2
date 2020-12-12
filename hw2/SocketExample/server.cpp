3c9d62dfd206b23e3cfc129056bc79133282894d
y10ab1 <b07902144@ntu.edu.tw>
2020-12-12 23:45:44 +0800

    ls

--- hw2/SocketExample/server.cpp
+++ hw2/SocketExample/server.cpp
@@ -259,6 +259,7 @@ int main(int argc, char **argv)
                             status[i] = 0;
 
                             cout << "end of ls\n";
+                            ff.close("list.txt");
                         }
                         cout << msg << endl;
                     }
